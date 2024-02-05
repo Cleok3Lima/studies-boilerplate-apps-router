f978edd475cc4d9231b808a40fafbfd4a6d9bb29
Cléo "Chii Kozume" Lima <cleolimadev@gmail.com>
2024-02-05 13:26:11 -0300

    fixes the tests

--- src/components/Main/test.tsx
+++ src/components/Main/test.tsx
@@ -4,12 +4,23 @@ import Main from '.'
 
 describe('<Main />', () => {
   it('should render the heading', () => {
-    // Renderiza o componente
-    render(<Main />)
+    // renderiza o component
+    const { container } = render(<Main />)
 
-    // Busca o elemento e verifica a existência dele
+    // busca o elemento e verifica a existência dele
     expect(
       screen.getByRole('heading', { name: /react avançado/i })
     ).toBeInTheDocument()
+
+    // gerar snapshot
+    expect(container.firstChild).toMatchSnapshot()
+  })
+
+  it('should render the colors correctly', () => {
+    // renderiza o component
+    const { container } = render(<Main />)
+
+    // verifica se o background-color está correto
+    expect(container.firstChild).toHaveStyle({ 'background-color': '#06092b' })
   })
 })
